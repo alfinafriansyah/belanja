4e379bd9877056dbb1c45a3c4d54211a9bf4175f
afriansyah <alfinafriansyah68@gmail.com>
2025-10-06 20:34:25 +0700

    feat: define model in home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/widgets.dart';
+import 'package:belanja/models/item.dart';
 
 class HomePage extends StatelessWidget{
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000)
+  ];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
